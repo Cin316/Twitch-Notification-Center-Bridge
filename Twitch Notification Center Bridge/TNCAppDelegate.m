ee720308d464b593859f8877bab2f512315c53fa
Nicholas Reichert <joe13reich@gmail.com>
2014-07-11 16:36:58 -0400

    Cleaned up initial codebase.

--- Twitch Notification Center Bridge/TNCAppDelegate.m
+++ Twitch Notification Center Bridge/TNCAppDelegate.m
@@ -2,9 +2,6 @@
 //  TNCAppDelegate.m
 //  Twitch Notification Center Bridge
 //
-//  Created by Nicholas Reichert on 7/11/14.
-//  Copyright (c) 2014 Nicholas Reichert. All rights reserved.
-//
 
 #import "TNCAppDelegate.h"
 
